7921723150f5eb4ee1c5d5fe57b088721d7a1ab3
zzyj79411-star <zzyj79411@gmail.com>
2026-03-21 22:53:43 +0800

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
